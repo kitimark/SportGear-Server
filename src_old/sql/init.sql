5b8c844f717866116b7b016793321b51565fd901
zNext666 <alonereview@gmail.com>
2019-06-11 01:37:06 +0700

    update readme.md

--- src_old/sql/init.sql
+++
@@ -1,4 +0,0 @@
--- require mariadb >= 10.3
-CREATE DATABASE gearsport;
-CREATE USER 'gearsport'@'localhost' IDENTIFIED BY 'Z2VhcnNwb3J0';
-GRANT select,insert,update,delete ON gearsport.* TO 'gearsport'@'localhost';
