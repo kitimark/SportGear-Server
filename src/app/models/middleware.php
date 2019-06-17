680de04cde964ac0f018f59e9db22442aebba314
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-06-17 07:06:04 +0700

    Add cors in header

--- src/app/models/middleware.php
+++ src/app/models/middleware.php
@@ -1,6 +1,7 @@
 <?php
 // Application middleware
 $app->add(new Tuupola\Middleware\JwtAuthentication([
+    "secure" => true,
     "secret" => $config['settings']['token']['key'],
     "ignore" => ["/api/v1/users/login","/api/v1/university/login","/user/test/add","/routes"],
     "callback" => function ($request, $response, $arguments) use ($container) {
@@ -16,3 +17,27 @@ $app->add(new Tuupola\Middleware\JwtAuthentication([
 ]));
 
 //HS512
+
+// TODO: setting up CORS http
+$app->add(function($request, $response, $next) {
+    $route = $request->getAttribute("route");
+    $methods = [];
+
+    if (!empty($route)) {
+        $pattern = $route->getPattern();
+
+        foreach ($this->router->getRoutes() as $route) {
+            if ($pattern === $route->getPattern()) {
+                $methods = array_merge_recursive($methods, $route->getMethods());
+            }
+        }
+        //Methods holds all of the HTTP Verbs that a particular route handles.
+    } else {
+        $methods[] = $request->getMethod();
+    }
+
+    $response = $next($request, $response);
+    return $response->withHeader("Access-Control-Allow-Methods", implode(",", $methods))
+                    ->withHeader("Access-Control-Allow-Origin", "*")
+                    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization');
+});
