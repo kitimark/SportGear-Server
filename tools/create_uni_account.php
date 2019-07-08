d3453a4cf708e1c6bbceac9b2e969ddf585f33f6
znext666 <alonereview@gmail.com>
2019-07-08 10:08:41 +0000

    Fixed tools to add a uni account

--- tools/create_uni_account.php
+++ tools/create_uni_account.php
@@ -1,12 +1,30 @@
 <?php
+function clean($string) {
+    $string = str_replace(' ', '', $string); // Replaces all spaces with hyphens.
+ 
+    return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+ }
+
 $uni = json_decode(file_get_contents(__DIR__.'/uni.json'),true);
 $keys = array_keys($uni);
-$mysqli = new mysqli("localhost", "root", "", "gearsport");
-for($index = 0 ; $index < count($keys) - 1 ; $index++){
-    $name = $uni[$keys[$index]];
-    $email = $name . '@localhost.com';
-    $hash = password_hash($name, PASSWORD_DEFAULT);
+$settings['host'] = "172.19.0.3";
+$settings['dbname'] = "gearsport";
+$settings['user'] = "root";
+$settings['pass'] = "1234";
+echo '(' . count($keys) . ')= ';
+for($index = 0 ; $index < count($keys) ; $index++){
+    $pdo = new PDO("mysql:host=" . $settings['host'] . ";dbname=" . $settings['dbname'],$settings['user'], $settings['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $name = clean($uni[$keys[$index]]);
+    $email = clean(str_replace(' ', '', strtolower($name))) . '@localhost.com';
+    $hash = password_hash(clean(str_replace(' ', '', strtolower($name))), PASSWORD_DEFAULT);
     $sql = "INSERT INTO account_uni(uni,email,uni_full_name,uni_pwd) VALUES ('$keys[$index]','$email','$name','$hash')";
-    $mysqli->query($sql);
-    echo 'INSERT LINE :' . $index . '</br>';
+    //$pdo->query($sql);
+    try{
+        $pdo->query($sql);
+        echo $index . '-> ';
+    }catch(PDOException $e){
+        echo "Error: " . $keys[$index] . "<br>" . $e->getMessage();
+    }
+    //echo 'INSERT LINE :' . $index . '</br>';
 }
