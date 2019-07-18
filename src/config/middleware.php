216f198f5d0ce1fd0b88a810245a00d29bbe65f1
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-07-18 23:16:05 +0000

    Revert "Disble bypass attribute jwt"
    This reverts commit de948c66c18ca0850b2a5795f7884a56a37195e7.

--- src/config/middleware.php
+++ src/config/middleware.php
@@ -30,12 +30,11 @@ $app->add(new Tuupola\Middleware\JwtAuthentication([
     "path" => ["/api"],
     "attribute" => "jwt",
     "ignore" => ["/api/v1/university/login"],
-    // this code not working
-    // "before" => function ($request, $arguments) {
-    //     $jwt = $request->getAttribute("jwt");
-    //     $roles = $jwt['roles'];
-    //     return $request->withAttribute("roles", $roles);//send a roles to next middleware
-    // },
+    "before" => function ($request, $arguments) {
+        $jwt = $request->getAttribute("jwt");
+        $roles = $jwt['roles'];
+        return $request->withAttribute("roles", $roles);//send a roles to next middleware
+    },
     "secret" => $config['settings']['token']['key'],
     "error" => function ($response, $arguments) {
         $data["status"] = "error";
