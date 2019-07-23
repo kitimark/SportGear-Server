8516336a8065a7d601d73ce16b9a540af7be8e55
zNext666 <alonereview@gmail.com>
2019-07-24 05:08:01 +0700

    Update database for identify account_uni & changeaccount no need to use unique email and can be null

--- sql/init.sql
+++ sql/init.sql
@@ -9,6 +9,8 @@ CREATE TABLE account_uni(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     uni VARCHAR(7) NOT NULL,
     email VARCHAR(255) NOT NULL,
+    owner_fname VARCHAR(255),
+    owner_lname VARCHAR(255),
     uni_full_name VARCHAR(100) NOT NULL,
     uni_pwd VARCHAR(255) NOT NULL,
     UNIQUE KEY(uni)
@@ -20,12 +22,11 @@ CREATE TABLE account(
     uni VARCHAR(7) NOT NULL,
     fname VARCHAR(128) NOT NULL,
     lname VARCHAR(128) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
     pwd VARCHAR(255) NOT NULL,
     img_url text,
     details JSON,
     CHECK (JSON_VALID(details)),
-    UNIQUE KEY (email),
     UNIQUE KEY (sid),
     FOREIGN KEY (uni) REFERENCES account_uni(uni)
 );
